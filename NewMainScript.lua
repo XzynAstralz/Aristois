16397b843d0e7f9dab5e9725a43c072002cfc0f8
XzynAstralz <xzynggx@gmail.com>
2024-08-27 15:59:29 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,1 +1,9 @@
-return loadstring(game:HttpGet('https://raw.githubusercontent.com/XzynAstralz/Aristois/main/MainScript.lua'))()
+local success, result = pcall(function()
+    return loadstring(game:HttpGet('https://raw.githubusercontent.com/XzynAstralz/Aristois/main/MainScript.lua'))()
+end)
+
+if not success then
+    warn("Failed to load the script: " .. tostring(result))
+else
+    return result
+end
